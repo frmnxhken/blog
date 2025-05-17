f762b4431ef57f62f45f4901934a7569b2dea18a
frmnxhken <frmnxhken@gmail.com>
2025-05-17 15:52:04 +0700

    Change color

--- frontend/src/pages/blog/home/ui/ListCard.jsx
+++ frontend/src/pages/blog/home/ui/ListCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const ListCard = ({ title, thumbnail, date }) => {
   return (
-    <div className="border-2 border-zinc-800 rounded-xl p-2">
+    <div className="border-2 border-zinc-200 rounded-xl p-2">
       <div className="flex justify-between gap-4">
         <div className="w-full">
           <img
@@ -13,7 +13,7 @@ const ListCard = ({ title, thumbnail, date }) => {
         </div>
         <div className="w-2/4 p-8">
           <div className="flex flex-col justify-between h-full">
-            <h3 className="text-white font-semibold text-4xl">{title}</h3>
+            <h3 className="font-semibold text-4xl">{title}</h3>
             <p className="text-zinc-500">{date}</p>
           </div>
         </div>
--- frontend/src/shared/ui/Navbar.jsx
+++ frontend/src/shared/ui/Navbar.jsx
@@ -3,12 +3,12 @@ import { HiBars2 } from "react-icons/hi2";
 
 const Navbar = () => {
   return (
-    <div className="w-full border-b-2 border-zinc-800 bg-zinc-950">
+    <div className="w-full border-b-2 border-zinc-200">
       <div className="container max-w-[1200px] mx-auto px-4 py-6 flex justify-between items-center">
         <div>
-          <h2 className="text-xl text-white font-semibold">Blog Us</h2>
+          <h2 className="text-xl font-semibold">Blog Us</h2>
         </div>
-        <HiBars2 color="white" size={32} />
+        <HiBars2 size={32} />
       </div>
     </div>
   );
